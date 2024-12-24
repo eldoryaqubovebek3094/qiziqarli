d06f821b2ab7a10305761135363c908f134f77db
eldoryaqubovebek3094 <ebek3094@gmail.com>
2024-12-24 09:19:02 +0500

    modal

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
+const modal = document.getElementById("myModal");
+// const btn = document.getElementById("openModal");
+const span = document.getElementsByClassName("close")[0];
+
+
 
 
 let currentLevel = 1;
@@ -108,7 +113,23 @@ document.getElementById("submit").addEventListener("click", () => {
   feedbackDiv.classList.remove("hidden");
 
   if (!allAnswered) {
-    alert("Iltimos, hamma kataklarga javoblarni yozing!");
+    // alert("Iltimos, hamma kataklarga javoblarni yozing!");
+  
+
+    
+        modal.style.display = "block";
+    
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
     return;
   }
 
@@ -146,6 +167,8 @@ document.getElementById("submit").addEventListener("click", () => {
         `;
   });
 });
+
+
 
 // Dastlabki misollarni generatsiya qilish
 generateQuestions(currentLevel);
